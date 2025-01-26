4d77745cd6f1e69ccbda72e73acb4589da44abeb
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2025-01-26 19:45:42 +0530

    Run download html script using bb

--- script.clj
+++ script.clj
@@ -1,3 +1,17 @@
 #!/usr/bin/env bb
 
-(println "Hello world")
+(require '[babashka.http-client :as http])
+
+(defn get-url [url]
+  (println "Downloading url : " url)
+  (http/get url))
+
+(defn write-html [file html]
+  (println "Writing files to html")
+  (spit file html))
+
+(let [[url file] *command-line-args*]
+  (when (or (empty? file) (empty? url))
+    (println "Usage : <url> <file>")
+    (System/exit 1))
+  (write-html file (:body (get-url url))))
