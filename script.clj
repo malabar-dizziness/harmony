7c9e7c1244425e1032931755ccb531a1b93d4ad9
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2025-01-26 19:57:20 +0530

    current file name script

--- script.clj
+++ script.clj
@@ -2,16 +2,24 @@
 
 (require '[babashka.http-client :as http])
 
-(defn get-url [url]
-  (println "Downloading url : " url)
-  (http/get url))
+;; (defn get-url [url]
+;;   (println "Downloading url : " url)
+;;   (http/get url))
 
-(defn write-html [file html]
-  (println "Writing files to html")
-  (spit file html))
+;; (defn write-html [file html]
+;;   (println "Writing files to html")
+;;   (spit file html))
 
-(let [[url file] *command-line-args*]
-  (when (or (empty? file) (empty? url))
-    (println "Usage : <url> <file>")
-    (System/exit 1))
-  (write-html file (:body (get-url url))))
+;; (let [[url file] *command-line-args*]
+;;   (when (or (empty? file) (empty? url))
+;;     (println "Usage : <url> <file>")
+;;     (System/exit 1))
+;;   (write-html file (:body (get-url url))))
+
+
+;; printing current filename
+
+(prn "The current file is : "*file*)
+
+
+;; parsing
